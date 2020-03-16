b76a4eb919c97055942e497ba480de9897ca6682
Mkhululi MN. Ncube <Mkhululi@NOOR-CAPITAL.LOCAL>
2020-03-16 15:31:16 +0400

    app and router update

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,10 +8,13 @@ router.post('/users', async (req,res)=>{
     try {
         const user = new User(req.body)
         await user.save()
+
 const token = await user.generateAuthToken()
 res.status(201).send({user, token})
+
     } catch (error) {
         res.status(400).send(error)
+        
     }
 })
 
